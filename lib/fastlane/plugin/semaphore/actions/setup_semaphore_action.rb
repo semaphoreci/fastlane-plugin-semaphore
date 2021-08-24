ea8934a879141a9ae14790ee116dd1ae713b0eb3
Aleksandar Mitrovic <61409859+AleksandarCole@users.noreply.github.com>
2021-08-24 14:50:12 +0200

    Update setup_semaphore_action.rb

--- lib/fastlane/plugin/semaphore/actions/setup_semaphore_action.rb
+++ lib/fastlane/plugin/semaphore/actions/setup_semaphore_action.rb
@@ -34,7 +34,7 @@ module Fastlane
           name: keychain_name,
           default_keychain: true,
           unlock: true,
-          timeout: 0,
+          timeout: false,
           lock_when_sleeps: true,
           add_to_search_list: true,
           password: ""
